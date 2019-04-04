ea5f76c5a6a5d4055d385f3e4de1a70d5f96e6e9
Frank Kusmiruk <48283695+FranKusmiruk@users.noreply.github.com>
2019-04-04 19:46:17 -0300

    Fix conflict with Skript's ExprIP (#1)

--- src/main/java/com/pikachu/webaddon/skript/expressions/ExprIP.java
+++ src/main/java/com/pikachu/webaddon/skript/expressions/ExprIP.java
@@ -8,11 +8,11 @@ import spark.Request;
 
 @Name("IP address")
 @Description("The IP address of a request")
-@Examples("send back \"Woah bro! Your IP address is %ip of event-request%\"")
+@Examples("send back \"Woah bro! Your IP address is %request-IP of event-request%\"")
 public class ExprIP extends SimplePropertyExpression<Request, String> {
 
 	static {
-		register(ExprIP.class, String.class, "ip", "requests");
+		register(ExprIP.class, String.class, "request(-| )IP", "requests");
 	}
 
 	@Override
